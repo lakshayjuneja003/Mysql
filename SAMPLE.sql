eda41cd98e2b482836f7e67a3625eda5b3e36ce0
lakshayjuneja003 <lakshayjuneja003@gmail.com>
2024-07-01 03:34:37 -0400

    Sql CRUD Opertions

---
+++ SAMPLE.sql
@@ -0,0 +1,21 @@
+SHOW databases;
+CREATE DATABASE if not exists Techielakshay;
+use techielakshay;
+select database();
+
+create TABLE employee(
+	EID		 INT PRIMARY KEY auto_increment,
+    FirstName varchar(20) NOT NULL,
+    LastName varchar(20) NOT NULL,
+    Age INT NOT NULL,
+    Salery INT NOT NULL,
+    Location varchar(30) NOT NULL
+);
+
+DESC employee;
+drop table employee;
+
+insert into employee values (1 , "lakshay " , "Juneja" , 18 , 200000, "Haryana");
+
+show tables;
+select * from employee;
