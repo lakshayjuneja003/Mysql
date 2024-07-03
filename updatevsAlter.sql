e218f9c1622c19126c5bb3a319259d5e96de560e
lakshayjuneja003 <lakshayjuneja003@gmail.com>
2024-07-03 04:04:39 -0400

    Update vs Alter

---
+++ updatevsAlter.sql
@@ -0,0 +1,19 @@
+/*
+
+update vs alter in sql
+
+update cmd or statement is used to maniplaute the data
+that is where concept of dml came menas
+ data manipluation language 
+
+but in case of alter statement alter statement 
+it is used to change the schema of the data
+schema - the way in which data was stored 
+
+so here came the concept of ddl
+ddl- data defination language
+
+*/
+
+update employee SET LastName = "Kumar " 
+where EID = 1;
